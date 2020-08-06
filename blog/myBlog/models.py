33c7b211470ae96ca33d8a880d80ef60e721d9bc
Sebastian Fernando Garcia Delgado <sefegade@gmail.com>
2020-08-06 11:13:46 -0500

    Creando Los modelos de las publicaciones

--- blog/myBlog/models.py
+++ blog/myBlog/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+	titulo = models.CharField(max_length = 255)
+	autor = models.ForeignKey(User, on_delete = models.CASCADE)
+	cuerpo = models.TextField()
+
+	def __str__(self):
+		return self.titulo + ' | ' + str(self.autor)
