f30ac766f19b140bf7edbbfd8f71c663bdbbf785
Sebastian Garcia <sefegade@gmail.com>
2020-08-15 13:18:15 -0500

    Agregando Titulo tag

--- blog/myBlog/models.py
+++ blog/myBlog/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Post(models.Model):
 	titulo = models.CharField(max_length = 255)
+	titulo_tag = models.CharField(max_length = 255, default="Blog")
 	autor = models.ForeignKey(User, on_delete = models.CASCADE)
 	cuerpo = models.TextField()
 
