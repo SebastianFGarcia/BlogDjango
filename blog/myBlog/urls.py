e47813e30bd287f075870eed486c0175dcaea396
Sebastian Garcia <sefegade@gmail.com>
2020-08-17 03:36:32 -0500

    Añadiendo Vista y Accion de eliminar

--- blog/myBlog/urls.py
+++ blog/myBlog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomeView, DetallePublicacion, CrearPublicacion ,ActualizarPublicacion
+from .views import HomeView, DetallePublicacion, CrearPublicacion, ActualizarPublicacion, EliminarPublicacion
 
 urlpatterns = [
 	path('', HomeView.as_view(), name = "home"),
 	path('post/<int:pk>', DetallePublicacion.as_view(), name = 'detalle-publicacion'),
 	path('create/', CrearPublicacion.as_view(), name = 'crear_publicacion'),
-	path('post/edit/<int:pk>',ActualizarPublicacion.as_view(), name= 'actualizar-publicacion')
+	path('post/edit/<int:pk>',ActualizarPublicacion.as_view(), name= 'actualizar-publicacion'),
+	path('post/<int:pk>/delete', EliminarPublicacion.as_view(), name= 'eliminar-publicacion')
 ]
--- blog/myBlog/views.py
+++ blog/myBlog/views.py
@@ -1,10 +1,12 @@
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 from .forms import PostForm, EditForm
 
 class HomeView(ListView):
 	model = Post
 	template_name = 'home.html'
+	ordering = 	['-id']
 
 
 class DetallePublicacion(DetailView):
@@ -20,3 +22,8 @@ class ActualizarPublicacion(UpdateView):
 	model = Post
 	template_name = 'Post/update.html'
 	form_class = EditForm 
+
+class EliminarPublicacion(DeleteView):
+	model = Post
+	template_name = 'Post/delete.html'
+	success_url = reverse_lazy('home')
