57d221eda192180ee8c7b9e28275ffbe3968d475
Sebastian Garcia <sefegade@gmail.com>
2020-08-20 22:53:45 -0500

    Añadiendo campos al registo

--- blog/members/views.py
+++ blog/members/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from .forms import RegistroUsuarioForm
 
 class RegistroUsuarioView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = RegistroUsuarioForm
     template_name = 'registration/registro.html'
     success_url = reverse_lazy('login')
